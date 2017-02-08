630ea7d9f58b040f2861663e0c2838a26d33032b
sungkook <sungkook@sungkooks-mbp.home>
2017-02-08 01:08:23 -0500

    initial commit

---
+++ src/main/java/com/genomestore/GenomeStoreApplication.java
@@ -0,0 +1,12 @@
+package com.genomestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenomeStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenomeStoreApplication.class, args);
+	}
+}
---
+++ src/test/java/com/genomestore/GenomeStoreApplicationTests.java
@@ -0,0 +1,16 @@
+package com.genomestore;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GenomeStoreApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
